5640be6fdfa6a9b2171643b6f117fe051d13f544
yamashm <awnmist@gmail.com>
2020-07-05 13:34:09 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 3: seller findAll ===");
+		list = sellerDao.findALl();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 	}
 
 }
